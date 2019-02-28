e77ed9a5a2d3558e31afa487409bd5013d7ceca4
Niols <niols@niols.fr>
2019-02-28 18:00:18 +0100

    from_string

--- src/problem.ml
+++ src/problem.ml
@@ -5,12 +5,25 @@ type t = (* FIXME *)
   { name : string }
 [@@deriving show]
 
-let from_file ~problem_name (filename : string) : t =
-  let ichan = open_in filename in
+let from_channel ~problem_name (ichan : in_channel) : t =
   ignore ichan;
   ignore problem_name;
-  ignore filename;
-  assert false (* FIXME *)
+  assert false
+
+let from_file ~problem_name (filename : string) : t =
+  let ichan = open_in filename in
+  let problem = from_channel ~problem_name ichan in
+  close_in ichan;
+  problem
+
+let from_string ~problem_name (string : string) : t =
+  let (ifd, ofd) = Unix.pipe () in
+  let ichan = Unix.in_channel_of_descr ifd in
+  let ochan = Unix.out_channel_of_descr ofd in
+  output_string ochan string;
+  let problem = from_channel ~problem_name ichan in
+  close_out ochan;
+  problem
 
 let name problem = problem.name
 
