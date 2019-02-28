96c88e3d6e9d1898a48f8ad6c527c751a03c0a99
Martin Pépin <kerl@wkerl.me>
2019-02-28 20:20:14 +0100

    algo naif terminé (pas testé)

--- src/naive.ml
+++ src/naive.ml
@@ -3,13 +3,36 @@ type available = {
   mutable nb_eligible: int;
 }
 
-let rec get_random_slide (rem : available) : Solution.slide * int =
+let rec get_random_slide_aux (rem : available) : Solution.slide * int =
   let n = Random.int (Array.length rem.slides) in
   let (slide, eligible) = rem.slides.(n) in
   if not eligible then
-    get_random_slide rem
+    get_random_slide_aux rem
   else
     (slide, n)
+
+let array_filter p from_ to_ =
+  let pos = ref 0 in
+  let append x =
+    to_.(!pos) <- x;
+    incr pos
+  in
+  Array.iter (fun x -> if p x then append x) from_;
+  !pos
+
+
+let get_random_slide rem =
+  let res = get_random_slide_aux rem in
+  rem.nb_eligible <- pred rem.nb_eligible;
+  if rem.nb_eligible > 10 && (rem.nb_eligible * 2 <= Array.length rem.slides) then begin
+    let arr = Array.make rem.nb_eligible rem.slides.(0) in
+    let nb_copies = array_filter snd rem.slides arr in
+    assert (nb_copies = rem.nb_eligible);
+    rem.slides <- arr;
+    res
+  end else
+    res
+
 
 let discard (rem: available) index =
   let (s, _) = rem.slides.(index) in
@@ -20,10 +43,12 @@ let rec fold_int f init = function
   | 0 -> init
   | n -> fold_int f (f init n) (n - 1)
 
-let keep_going _input _rem = assert false
+let keep_going rem =
+  rem.nb_eligible > 0
 
-let make_possible_slides _input : available =
-  assert false
+let make_possible_slides input : available =
+  let slides = SlidesFromPhotos.stupid input |> Array.of_list in
+  { slides = slides ; nb_eligible = Array.length slides }
 
 let solver input nb_iterations =
   let remaining_slides = make_possible_slides input in
@@ -53,7 +78,7 @@ let solver input nb_iterations =
     incr pos;
     discard remaining_slides index;
 
-    if keep_going input remaining_slides then solve index;
+    if keep_going remaining_slides then solve index;
   in
 
   solve index;
