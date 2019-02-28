3a437ac646b636b9a723ebf776eb232765582510
Victor Lanvin <victor.lanvin@irif.fr>
2019-02-28 19:15:28 +0100

    Score of slides

--- src/extPervasives.ml
+++ src/extPervasives.ml
@@ -31,3 +31,27 @@ let union_sorted l1 l2 =
         aux (h1::acc) t1 t2
   in
   aux [] l1 l2
+
+let rec common_elts l1 l2 = 
+  match l1, l2 with
+  | _, [] -> 0
+  | [], _ -> 0
+  | h1::t1, h2::t2 ->
+    if h1 < h2 then
+      common_elts t1 l2
+    else if h1 > h2 then
+      common_elts l1 t2
+    else
+      1 + (common_elts t1 t2)
+
+let rec non_common_elts l1 l2 = 
+  match l1, l2 with
+  | l1, [] -> List.length l1
+  | [], _ -> 0
+  | h1::t1, h2::t2 ->
+    if h1 < h2 then
+      1 + non_common_elts t1 l2
+    else if h1 > h2 then
+      non_common_elts l1 t2
+    else
+      non_common_elts t1 t2
--- src/solution.ml
+++ src/solution.ml
@@ -18,6 +18,20 @@ let to_file (filename : string) (solution : t) : unit =
   ignore solution;
   assert false (* FIXME *)
 
+let tags_of_slide = function
+  | One photo -> photo.Problem.tags
+  | Two (photo1, photo2) -> 
+    ExtPervasives.union_sorted photo1.Problem.tags photo2.Problem.tags
+
+let score_of_slides s1 s2 = 
+  let t1, t2 = tags_of_slide s1, tags_of_slide s2 in
+  let common_tags = ExtPervasives.common_elts t1 t2 in
+  let score1, score2 = 
+    ExtPervasives.non_common_elts t1 t2, 
+    ExtPervasives.non_common_elts t2 t1
+  in
+  min common_tags (min score1 score2)
+
 let score problem solution =
   ignore problem;
   ignore solution;
