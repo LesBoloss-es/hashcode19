f5a499e948f05e5a3df68cf8be414c08338b78d3
Niols <niols@niols.fr>
2019-02-28 21:32:09 +0100

    strict mode

--- src/config.ml
+++ src/config.ml
@@ -3,6 +3,7 @@ open ExtPervasives
 let loglevel = ref Logs.Debug
 let problems = ref "problems"
 let solutions = ref "solutions"
+let strict = ref false
 let workers = ref 8
 
 let set_level level =
@@ -10,12 +11,18 @@ let set_level level =
   | Ok (Some level) -> loglevel := level
   | _ -> raise (Arg.Bad "bad log level")
 
+let set_strict = function
+  | "yes" -> strict := true
+  | "no" -> strict := false
+  | _ -> raise (Arg.Bad "bad strict")
+
 let specs =
   let open Arg in
   align [
     "--log-level", String set_level,     spf "LEVEL Sets log level to LEVEL (default: %s)" (Logs.level_to_string (Some !loglevel)) ;
     "--problems",  Set_string problems,  spf "DIR Sets the problems directory to DIR (default: %s)" !problems ;
     "--solutions", Set_string solutions, spf "DIR Sets the solutions directory to DIR (default: %s)" !solutions ;
+    "--strict",    String set_strict,    spf "yes/no Fail on first error" ;
     "--workers",   Set_int workers,      spf "NB Sets the number of workers to NB (default: %d)" !workers ;
   ]
 
